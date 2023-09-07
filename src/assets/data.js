fd076570b6e75e813bea5000b19d6cb263a5e205
diego <dsruggeri@gmail.com>
2023-09-06 21:05:19 -0300

    correccion rutas imagenes en local

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,31 +4,31 @@ export const data = {
         {
         "nombre":"Diego Sebastián",
         "edad":36,
-        "foto":"./fotos/diego.png"
+        "foto":"/fotos/diego.png"
         },
         {
         "nombre":"Pablo",
         "edad":46,
-        "foto":"../src/assets/fotos/pablo.png"
+        "foto":"/fotos/pablo.png"
         },
         {
         "nombre":"Ariana",
         "edad":46,
-        "foto":"../src/assets/fotos/ariana.png"
+        "foto":"/fotos/ariana.png"
         },
         {"nombre":"Ágota",
         "edad":88,
-        "foto":"../src/assets/fotos/agota.png"
+        "foto":"/fotos/agota.png"
         },
         {
         "nombre":"Aurora",
         "edad":101,
-        "foto":"../src/assets/fotos/aurora.png"
+        "foto":"/fotos/aurora.png"
         },
         {
         "nombre":"Martín",
         "edad":66,
-        "foto":"../src/assets/fotos/martin.png"
+        "foto":"/fotos/martin.png"
         }
     ],
     "descripcion":"Un grupo con horarios chinos"    
